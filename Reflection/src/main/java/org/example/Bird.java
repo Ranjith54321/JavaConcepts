fb3e3ea1b026ddd663b6d95c26c9583bf5cebb34
ranjith.s <ranjith.s@gdn-commerce.com>
2025-05-25 10:45:53 +0530

    Reflection notes

---
+++ Reflection/src/main/java/org/example/Bird.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ * Created by Ranjith S on 01/01/25.
+ **/
+public class Bird {
+    public String name;
+    String country;
+}
---
+++ Reflection/src/main/java/org/example/Eagle.java
@@ -0,0 +1,27 @@
+package org.example;
+
+/**
+ * Created by Ranjith S on 01/01/25.
+ **/
+public class Eagle {
+    private Eagle() {
+        // private constructor
+    }
+    public void fly() {
+        System.out.println("fly");
+    }
+}
+
+
+/*
+    public String breed;
+    private boolean canSwim;
+    private boolean alive;
+    String nickName;
+    public void fly() {
+        System.out.println("fly");
+    }
+    private void eat() {
+        System.out.println("eat");
+    }
+ */
